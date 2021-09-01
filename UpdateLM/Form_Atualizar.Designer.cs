4e2108f72f625f95f7fcf15326f21ad35b1c480b
Luis Eduardo Ferreira Gomes <soloobr@gmail.com>
2021-08-31 22:27:29 -0300

    - update new

--- UpdateLM/Form_Atualizar.Designer.cs
+++ UpdateLM/Form_Atualizar.Designer.cs
@@ -29,46 +29,78 @@ namespace UpdateLM
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
+            this.lblatualizar = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.lblCountDown = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // lblatualizar
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Poppins", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(254, 119);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(278, 37);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Atualizando o Sistema ...";
+            this.lblatualizar.AutoSize = true;
+            this.lblatualizar.Font = new System.Drawing.Font("Poppins", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblatualizar.Location = new System.Drawing.Point(280, 83);
+            this.lblatualizar.Name = "lblatualizar";
+            this.lblatualizar.Size = new System.Drawing.Size(278, 37);
+            this.lblatualizar.TabIndex = 0;
+            this.lblatualizar.Text = "Atualizando o Sistema ...";
+            this.lblatualizar.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
             this.pictureBox1.Image = global::UpdateLM.Properties.Resources.icologo;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 50);
+            this.pictureBox1.Location = new System.Drawing.Point(20, 20);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(214, 230);
+            this.pictureBox1.Size = new System.Drawing.Size(214, 232);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(268, 159);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(301, 23);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // lblCountDown
+            // 
+            this.lblCountDown.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblCountDown.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCountDown.Location = new System.Drawing.Point(234, 229);
+            this.lblCountDown.Name = "lblCountDown";
+            this.lblCountDown.Size = new System.Drawing.Size(353, 23);
+            this.lblCountDown.TabIndex = 3;
+            this.lblCountDown.Text = "5";
+            this.lblCountDown.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblCountDown.Visible = false;
             // 
             // Atualizando
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(607, 330);
+            this.ClientSize = new System.Drawing.Size(607, 272);
+            this.Controls.Add(this.lblCountDown);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblatualizar);
             this.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Atualizando";
-            this.Padding = new System.Windows.Forms.Padding(0, 50, 0, 50);
+            this.Padding = new System.Windows.Forms.Padding(20);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Atualizando";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Atualizando_FormClosed);
             this.Load += new System.EventHandler(this.Atualizando_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -78,8 +110,11 @@ namespace UpdateLM
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblatualizar;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label lblCountDown;
     }
 }
 
--- UpdateLM/Form_Atualizar.cs
+++ UpdateLM/Form_Atualizar.cs
@@ -16,36 +16,92 @@ namespace UpdateLM
 {
     public partial class Atualizando : Form
     {
+        private int counter = 5;
+
         public Atualizando()
         {
             InitializeComponent();
-
-            //WebClient webClient = new WebClient();
-            var client = new WebClient();
-
-            try
-            {
-                System.Threading.Thread.Sleep(5000);
-                File.Delete(@".\LM-Financiamentos.exe");
-                client.DownloadFile("https://lmfinanciamentos.com.br/update/update.zip", @"update.zip");
-                string zipPath = @".\update.zip";
-                string extractPath = @".\";
-                ZipFile.ExtractToDirectory(zipPath, extractPath);
-                File.Delete(@".\update.zip");
-                Process.Start(@".\LM-Financiamentos.exe");
-                Close();
-
-            }
-            catch
-            {
-                Process.Start(@".\LM-Financiamentos.exe");
-                Close();
-            }
-}
+        }
 
         private void Atualizando_Load(object sender, EventArgs e)
         {
+            var client = new WebClient();
 
+            System.Threading.Thread.Sleep(5000);
+            if (File.Exists(@".\LM-Financiamentos.exe"))
+            {
+              File.Delete(@".\LM-Financiamentos.exe");
+            }
+            if (File.Exists(@".\update.zip"))
+            {
+                File.Delete(@".\update.zip");
+            }
+
+            client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(client_DownloadProgressChanged);
+            client.DownloadFileCompleted += new AsyncCompletedEventHandler(client_DownloadFileCompleted);
+            // Starts the download
+            client.DownloadFileAsync(new Uri("https://lmfinanciamentos.com.br/update/update.zip"), @".\update.zip");
+            
+            //System.Threading.Thread.Sleep(5000);
+                //File.Delete(@".\LM-Financiamentos.exe");
+                ////client.DownloadFile("https://lmfinanciamentos.com.br/update/update.zip", @"update.zip");
+                //string zipPath = @".\update.zip";
+                //string extractPath = @".\";
+                //ZipFile.ExtractToDirectory(zipPath, extractPath);
+                //File.Delete(@".\update.zip");
+                //Process.Start(@".\LM-Financiamentos.exe");
+                ////Close();
+                //MessageBox.Show("Atualização Feita Com Sucesso!");
+            //}
+            //catch
+           // {
+                //Process.Start(@".\LM-Financiamentos.exe");
+                //Close();
+             //   MessageBox.Show("Não fez!");
+            //}
+
+        }
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            double bytesIn = double.Parse(e.BytesReceived.ToString());
+            double totalBytes = double.Parse(e.TotalBytesToReceive.ToString());
+            double percentage = bytesIn / totalBytes * 100;
+
+            progressBar1.Value = int.Parse(Math.Truncate(percentage).ToString());
+        }
+        void client_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            string zipPath = @".\update.zip";
+            string extractPath = @".\";
+            ZipFile.ExtractToDirectory(zipPath, extractPath);
+            lblatualizar.Text = "Sistema Atualizado!";
+            timer.Enabled = true;
+            lblCountDown.Visible = true;
+
+        }
+
+        private void Atualizando_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            File.Delete(@".\update.zip");
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            counter--;
+            if (counter == -1)
+            {
+                if (File.Exists(@".\LM-Financiamentos.exe"))
+                {
+                    Process.Start(@".\LM-Financiamentos.exe");
+                    Close();
+                }
+                timer.Stop();
+
+            }
+                
+            lblCountDown.Text = counter.ToString();
+
+           
         }
     }
 }
